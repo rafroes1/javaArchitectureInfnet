3bfaf99e5f217ec7470772fb44ece2287dc38a0d
Rafa PC <Rafa PC@DESKTOP-HAFATGG>
2023-10-25 10:31:38 -0300

    Up to date com aula 24/10

--- appvenda/src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
+++ appvenda/src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
@@ -2,11 +2,19 @@ package br.edu.infnet.appvenda.model.domain;
 
 import java.util.List;
 
+import javax.persistence.*;
+
+@Entity //cria essa classe como tabela
+@Table(name = "TVendedor") //caso nao queria que a table tenha o nome exatamente igual a da classe voce pode usar essa anotação pra redefinir
 public class Vendedor {
 	
+	@Id //primary key id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment do primary key
+	private Integer id;
 	private String nome;
 	private String cpf;
 	private String email;
+	@Transient //desconsidera esse campo na criação da tabela
 	private List<Produto> produtos;
 	
 	public String getNome() {
--- appvenda/src/main/java/br/edu/infnet/appvenda/model/service/VendedorService.java
+++ appvenda/src/main/java/br/edu/infnet/appvenda/model/service/VendedorService.java
@@ -4,19 +4,24 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.edu.infnet.appvenda.model.domain.Vendedor;
+import br.edu.infnet.appvenda.model.repository.VendedorRepository;
 
 @Service
 public class VendedorService {
-	private Map<String, Vendedor> mapaVendedor = new HashMap<String, Vendedor>();
+	//TODO: Fazer para as outras classes
+	
+	@Autowired
+	private VendedorRepository repository;
 	
 	public void addVendedor(Vendedor vendedor) {
-		mapaVendedor.put(vendedor.getCpf(), vendedor);
+		repository.save(vendedor);
 	}
 	
 	public Collection<Vendedor> getVendedorList(){
-		return mapaVendedor.values();
+		return (Collection<Vendedor>) repository.findAll();
 	}
 }
